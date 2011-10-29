eafa5065c5c5ea2b0edb5df33b3c4829d3b01b7f
Shawn Ostler <shawn@inclusvedv.com>
2011-10-29 16:03:40 -0600

    Moved anytime links

--- Inclusive/View/Helper/DateTimePicker.php
+++ Inclusive/View/Helper/DateTimePicker.php
@@ -6,9 +6,9 @@ class Inclusive_View_Helper_DateTimePicker extends Zend_View_Helper_FormText {
 		
 		$xhtml = parent::formText($name,$value,$attribs);
 		
-		$this->view->headLink()->appendStylesheet('/css/anytime.css');
+		$this->view->headLink()->appendStylesheet('/anytime/anytime.css');
 		
-		$this->view->JQuery()->addJavascriptFile('/js/anytime.js');
+		$this->view->JQuery()->addJavascriptFile('/anytime/anytime.js');
 		
 		$this->view->JQuery()->addOnload('AnyTime.picker("'.$name.'",{format:"%c/%e/%Y %h:%i:%s %p"});');
 		
