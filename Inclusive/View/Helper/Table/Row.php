cf225205fa7d7588f68864b7cf109b2e366a66b1
Shawn Ostler <shawn@inclusvedv.com>
2011-11-06 18:40:20 -0700

    Added Auth_User and Table

--- Inclusive/View/Helper/Table/Row.php
+++ Inclusive/View/Helper/Table/Row.php
@@ -2,17 +2,53 @@
 
 class Inclusive_View_Helper_Table_Row extends Zend_View_Helper_Abstract {
 	
-	public function row(array $row,$options=null) {
+	public function row($row,$options=null) {
 		
-		$string = '<tr class="'.((isset($options['class'])) ? $options['class'] : '').'">';
+		$string = '';
 		
-		foreach ($row as $key => $value) {
+		if (is_array($row)) {
 			
-			$string .= '<td class"'.$key.'">'.$value.'</td>';
+			$string .= '<tr class="'.((isset($options['class'])) ? $options['class'] : '').'">';
+			
+			foreach ($row as $key => $value) {
+				
+				$string .= '<td class"'.$key.'">'.$value.'</td>';
+				
+			}
+			
+			$string .= '</tr>';
+			
+		} elseif ($row instanceof Inclusive_Table_Row) {
+			
+			$string .= '<tr class="'.(($row->getOption('class')) ? $row->getOption('class') : '').'">';
+			
+			foreach ($row->getFields() as $field) {
+				
+				$string .= '<td class="'.strtolower($field).'">';
+				
+				$value = $row->getValue($field);
+				
+				if ($value instanceof Zend_Navigation) {
+					
+					$string .= $this->view->navigation()->menu($value)->render();
+					
+				} else {
+					
+					$string .= $value;
+					
+				}
+				
+				$string .= '</td>';
+				
+			}
+			
+			$string .= '</tr>';
+			
+		} else {
+			
+			$string .= $row;
 			
 		}
-		
-		$string .= '</tr>';
 		
 		return $string;
 		
