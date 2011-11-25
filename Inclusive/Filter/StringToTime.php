bc10b8325f1b5426135f5dab8e8182a5c41f1143
Sosy <sosy@inclusivedv.com>
2011-11-24 20:52:54 -0700

    Add user interface

--- Inclusive/Filter/StringToTime.php
+++ Inclusive/Filter/StringToTime.php
@@ -4,6 +4,12 @@ class Inclusive_Filter_StringToTime implements Zend_Filter_Interface {
 	
 	public function filter($value) {
 		
+		if (is_int($value)) {
+			
+			return $value;
+			
+		}
+		
 		$result = strtotime($value);
 		
 		if ($result) {
@@ -11,8 +17,6 @@ class Inclusive_Filter_StringToTime implements Zend_Filter_Interface {
 			return $result;
 			
 		}
-		
-		return $value;
 		
 	}
 	
