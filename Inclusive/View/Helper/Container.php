815a2b73db06e60b762ac35f421e966350ef2674
Sosy <sosy@inclusivedv.com>
2011-11-28 11:01:04 -0700

    Changes to User/Row/Container

--- Inclusive/View/Helper/Container.php
+++ Inclusive/View/Helper/Container.php
@@ -4,24 +4,33 @@ class Inclusive_View_Helper_Container extends Zend_View_Helper_Abstract {
 	
 	public function container($content,$options=null) {
 		
-		$string = '';
+		$string = '<div class="container '.(isset($options['class']) ? $options['class'] : '').'">'."\n";
 		
+        if (isset($options['wrapper'])
+            && $options['wrapper']) {
+            
+            $string .= '<div class="wrapper">'."\n";
+            
+        }
+        
 		if (isset($options['title'])) {
 			
 			$string .= '<h3 class="title">'.$options['title'].'</h3>';
 			
 		}
 		
-		$string .= $content;
-		
+        $string .= $content;
+        
         if (isset($options['wrapper'])
             && $options['wrapper']) {
             
-            $string = '<div class="wrapper">'.$string.'</div>';
+            $string .= '<div class="clear"></div>';
+            	
+            $string .= '</div>'."\n";
             
         }
         
-		$string = '<div class="container '.(isset($options['class']) ? $options['class'] : '').'">'.$string.'</div>';
+		$string .= '</div>'."\n";
 		
 		return $string;
 		
