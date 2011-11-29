8b2552f35e781b3c0055c8fdf45f931a751b4cc0
Sosy <sosy@inclusivedv.com>
2011-11-28 17:34:07 -0700

    Added support for mulitple databases

--- Inclusive/Db/Table/Abstract.php
+++ Inclusive/Db/Table/Abstract.php
@@ -4,6 +4,8 @@ abstract class Inclusive_Db_Table_Abstract extends Zend_Db_Table_Abstract {
 	
 	protected $_module = null;
 	
+	protected $_multiDb = null;
+	
 	public function service($name,$module=null) {
 		
 		if (!$module) {
@@ -13,6 +15,30 @@ abstract class Inclusive_Db_Table_Abstract extends Zend_Db_Table_Abstract {
 		}
 		
 		return Inclusive_Locator::service($name,$module);
+		
+	}
+	
+	protected function _setupDatabaseAdapter() {
+		
+		if ($this->_multiDb) {
+			
+			$multiDb = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('multidb');
+			
+			if ($multiDb) {
+				
+				$adapter = $multiDb->getDb($this->_multiDb);
+				
+				if ($adapter instanceof Zend_Db_Adapter_Abstract) {
+					
+					return $this->_db = $adapter;
+					
+				}
+				
+			}
+			
+		}
+		
+		return parent::_setupDatabaseAdapter();
 		
 	}
 	
