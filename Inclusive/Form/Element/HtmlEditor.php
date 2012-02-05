97bf0ab7a94a87bc75317e70137b64175342a771
Sosy <sosy@inclusivedv.com>
2012-02-05 02:02:01 -0700

    Added new Element/HtmlEditor

---
+++ Inclusive/Form/Element/HtmlEditor.php
@@ -0,0 +1,17 @@
+<?php
+
+class Inclusive_Form_Element_HtmlEditor extends Zend_Form_Element_Textarea {
+
+	public function __contruct($spec,$options=null) {
+	
+		parent::__contruct($spec,$options);
+		
+		if ($this->getFilter('Inclusive_Filter_HtmlPurifier')) {
+		
+			$this->addFilter(new Inclusive_Filter_HtmlPurifier());
+		
+		}
+		
+	}
+
+}
