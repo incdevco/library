7b056eceb65d5eb95f4d13168e857aa932d16430
Sosy <sosy@inclusivedv.com>
2012-05-07 21:43:13 -0600

    Removed Abstract Requires to allow flexibility

--- Inclusive/Service/Adapter/Abstract.php
+++ Inclusive/Service/Adapter/Abstract.php
@@ -4,16 +4,6 @@ abstract class Inclusive_Service_Adapter_Abstract {
 
 	protected $_service = null;
 	
-	abstract public function add(array $clean);
-
-	abstract public function createUniqueId($length);
-	
-	abstract public function delete($where);
-	
-	abstract public function edit(array $clean,$where=null);
-	
-	abstract public function get($where);
-
 	public function getService() 
 	{
 	
