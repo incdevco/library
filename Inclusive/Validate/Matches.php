b778da6a359bdd21bb4391e9c0f42c428fa25e49
Sosy <sosy@inclusivedv.com>
2012-05-22 09:16:17 -0600

    Added Matches Validator

---
+++ Inclusive/Validate/Matches.php
@@ -0,0 +1,43 @@
+<?php
+
+class Inclusive_Validate_Matches extends Zend_Validate_Abstract
+{
+
+	protected $_elementToMatch = null;
+
+	protected $_messageTemplates = array(
+		'noElement'=>'Element To Match Does Not Exist',
+		'noMatch'=>'Does not match %value%'
+		);
+	
+	public function __construct($elementToMatch,$options=null)
+	{
+	
+		$this->_elementToMatch = $elementToMatch;
+		
+	}
+	
+	public function isValid($value,$context=null)
+	{
+	
+		if (!isset($context[$this->_elementToMatch]))
+		{
+		
+			$this->_error('noElement',$this->_elementToMatch);
+			return false;
+		
+		}
+	
+		if ($value == $context[$this->_elementToMatch])
+		{
+		
+			return true;
+		
+		}
+		
+		$this->_error('noMatch');
+		return false;
+	
+	}
+	
+}
