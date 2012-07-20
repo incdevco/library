73fc17542f0d6ea52eb835eea95b0fbea15f0d09
Sosy <sosy@inclusivedv.com>
2012-07-20 01:43:36 -0600

    Added Hidden Element

--- Inclusive/Form.php
+++ Inclusive/Form.php
@@ -1,7 +1,50 @@
 <?php
 
-class Inclusive_Form extends Zend_Form {
+class Inclusive_Form extends Zend_Form 
+{
 	
+	protected $_services = array();
 	
+	protected $_serviceClasses = array();
+	
+	public function getService($key) 
+	{
+	
+		if (isset($this->_serviceClasses[$key]))
+		{
+		
+			$class = $this->_serviceClasses[$key];
+		
+		}
+		else 
+		{
+		
+			$class = $key;
+		
+		}
+	
+		if (!isset($this->_services[$class])
+			or !($this->_services[$class] instanceof $class))
+		{
+		
+			$this->setService(new $class(),$key);
+		
+		}
+		
+		return $this->_services[$key];
+		
+	}
+	
+	public function setService(
+		Inclusive_Service_Abstract $service,
+		$key
+	) 
+	{
+	
+		$this->_services[$key] = $service;
+			
+		return $this;
+		
+	}
 	
 }
