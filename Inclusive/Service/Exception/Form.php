93ffc72b276b88d27889830c1e7561d1174acbe3
Sosy <sosy@inclusivedv.com>
2012-09-18 16:37:30 -0600

    Removed blank errors from message

--- Inclusive/Service/Exception/Form.php
+++ Inclusive/Service/Exception/Form.php
@@ -1,16 +1,11 @@
 <?php
 
-class Inclusive_Service_Exception_Form 
-	extends Inclusive_Service_Exception 
+class Inclusive_Service_Exception_Form extends Inclusive_Service_Exception 
 {
 	
 	protected $_form = null;
 	
-	public function __construct(
-		$msg='',
-		$code=0,
-		Exception $previous=null
-	) 
+	public function __construct($msg='',$code=0,Exception $previous=null) 
 	{
 	
 		if ($msg instanceof Zend_Form)
@@ -18,28 +13,23 @@ class Inclusive_Service_Exception_Form
 		
 			$this->setForm($msg);
 			
-			$msg = get_class($msg).' is Invalid. ';
+			$msg = get_class($msg).' is Invalid';
 			
-			foreach ($this->getForm()->getElements() as $element)
-			{
-			
-				$msg .= print_r($element->getErrorMessages(),true);
-			
-			}	
-		
 		}
 	
 		parent::__construct($msg,$code,$previous);
 	
 	}
 	
-	public function getForm() {
+	public function getForm() 
+	{
 	
 		return $this->_form;
 		
 	}
 	
-	public function setForm(Zend_Form $form) {
+	public function setForm(Zend_Form $form) 
+	{
 	
 		$this->_form = $form;
 		
