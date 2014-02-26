45279ed846656408ed7daaeb57923aa288925e1e
Shawn Ostler <shawn@inclusivedv.com>
2014-02-26 16:11:37 -0700

    add magic functions to service abstract

--- Inclusive/Service/Abstract.php
+++ Inclusive/Service/Abstract.php
@@ -13,6 +13,8 @@ abstract class Inclusive_Service_Abstract
 	
 	protected $_modelClass = null;
 	
+	protected $_primary = null;
+	
 	protected $_setClass = null;
 	
 	protected $_service = null;
@@ -240,21 +242,204 @@ abstract class Inclusive_Service_Abstract
 	
 	abstract public function isAllowed($model,$privilege);
 	
-	public function _throw($message)
+	protected function _add(array $data)
+	{
+	
+		$form = $this->getForm('Add');
+		
+		if ($form->isValid($data))
+		{
+		
+			$clean = $form->getValues();
+			
+			$model = $this->createModel($clean);
+			
+			$privilege = 'add';
+			
+			if ($this->isAllowed($model,$privilege))
+			{
+			
+				return $this->getAdapter()->add($clean);
+			
+			}
+			
+			return $this->_throwNotAllowed($model,$privilege);
+		
+		}
+		
+		return $this->_throwForm($form);
+	
+	}
+	
+	protected function _delete(array $data)
+	{
+	
+		$form = $this->getForm('Delete');
+		
+		if ($form->isValid($data))
+		{
+		
+			$clean = $form->getValues();
+			
+			$model = $this->fetchOne($clean);
+			
+			$privilege = 'delete';
+			
+			if ($this->isAllowed($model,$privilege))
+			{
+			
+				return $this->getAdapter()->delete($clean);
+			
+			}
+			
+			return $this->_throwNotAllowed($model,$privilege);
+		
+		}
+		
+		return $this->_throwForm($form);
+	
+	}
+	
+	protected function _edit(array $data)
+	{
+	
+		$form = $this->getForm('Edit');
+		
+		if ($form->isValid($data))
+		{
+		
+			$clean = $form->getValues();
+			
+			$model = $this->fetchOne($clean);
+			
+			$privilege = 'edit';
+			
+			if ($this->isAllowed($model,$privilege))
+			{
+				
+				$primary = $this->_primary;
+				
+				if (!is_array($primary))
+				{
+				
+					$primary = array($primary);
+				
+				}
+				
+				$where = array();
+				
+				foreach ($primary as $key)
+				{
+				
+					$where[$key] = $clean[$key];
+					
+					unset($clean[$key]);
+				
+				}
+				
+				return $this->getAdapter()->edit($clean,$where);
+			
+			}
+			
+			return $this->_throwNotAllowed($model,$privilege);
+		
+		}
+		
+		return $this->_throwForm($form);
+	
+	}
+	
+	public function _fetchAll(array $data)
+	{
+	
+		$form = $this->getForm('FetchAll');
+		
+		if ($form->isValid($data))
+		{
+		
+			$clean = $form->getValues();
+			
+			$results = $this->getAdapter()->fetchAll($clean);
+			
+			$set = $this->createSet();
+			
+			$privilege = 'view';
+			
+			foreach ($results as $result)
+			{
+			
+				$model = $this->createModel($result);
+				
+				if ($this->isAllowed($model,$privilege))
+				{
+				
+					$set->addModel($model);
+				
+				}
+			
+			}
+			
+			return $set;
+		
+		}
+		
+		return $this->_throwForm($form);
+	
+	}
+	
+	protected function _fetchOne(array $data)
+	{
+	
+		$form = $this->getForm('FetchOne');
+		
+		if ($form->isValid($data))
+		{
+		
+			$clean = $form->getValues();
+			
+			$result = $this->getAdapter()->fetchRow($clean);
+			
+			if ($result)
+			{
+			
+				$model = $this->createModel($result);
+				
+				$privilege = 'view';
+				
+				if ($this->isAllowed($model,$privilege))
+				{
+				
+					return $model;
+				
+				}
+				
+				return $this->_throwNotAllowed($model,$privilege);
+			
+			}
+			
+			return $this->_throw('No Model Found');
+		
+		}
+		
+		return $this->_throwForm($form);
+	
+	}
+	
+	protected function _throw($message)
 	{
 	
 		throw new Inclusive_Service_Exception($message);
 	
 	}
 	
-	public function _throwForm(Zend_Form $form)
+	protected function _throwForm(Zend_Form $form)
 	{
 	
 		throw new Inclusive_Service_Exception_Form($form);
 	
 	}
 	
-	public function _throwNotAllowed($model,$privilege)
+	protected function _throwNotAllowed($model,$privilege)
 	{
 	
 		throw new Inclusive_Service_Exception_NotAllowed($model,$privilege);
