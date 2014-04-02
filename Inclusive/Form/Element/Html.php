f3a74c068a08313dbfc5320e66f8e7c3f0e75cd5
Shawn Ostler <shawn@incdev.co>
2014-04-02 17:19:51 -0600

    html

---
+++ Inclusive/Form/Element/Html.php
@@ -0,0 +1,20 @@
+<?php
+
+class Inclusive_Form_Element_Html extends Zend_Form_Element_Textarea 
+{
+
+	public function __contruct($spec,$options=null) 
+	{
+	
+		parent::__contruct($spec,$options);
+		
+		if (!$this->getFilter('Inclusive_Filter_HtmlPurifier')) 
+		{
+		
+			$this->addFilter(new Inclusive_Filter_HtmlPurifier());
+		
+		}
+		
+	}
+
+}
